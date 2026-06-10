71e2f828d429d0166c5c2cb3ed897bc68034d325
Nisha Yadav <nishayadav1703@gmail.com>
2026-06-10 11:23:06 +0530

    Java literals or data type explain

---
+++ FirstJavaProgramCWH/src/CWH_04_literals.java
@@ -0,0 +1,16 @@
+public class CWH_04_literals {
+    static void main(String[] args) {
+        byte age = 34;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        boolean a = true;
+        int age2 = 56;
+        long ageDino = 566666666666666L;
+        short age3 = 67;
+        System.out.println(age);
+
+        String str = "Harry";
+        System.out.println(str);
+    }
+}
